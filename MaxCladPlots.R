e6abee92f8dd6ed8aee180ccdaf8a3b9525f71c5
Keith Bouma-Gregson <kbg@berkeley.edu>
2013-10-23 14:33:55 -0700

    MaxCladPlots
    Plots of the Max Clad height and light, flow, and depth

---
+++ MaxCladPlots.R
@@ -0,0 +1,64 @@
+# Script creates plots to visualize the relationship between maximum Clad length at each point across the transects and 1) light 2) substrate 3) flow 4) depth.
+
+# created by K. Bouma-Gregson Oct. 2013
+
+
+# Input max clad data (file = CladMaxPointWet.csv)
+
+		cml<-read.csv(file.choose())
+
+		
+
+  	
+# Plots of the Clad Max and Light data
+
+	library(ggplot2)
+	library(RColorBrewer)
+	#install.packages("gridExtra")
+	library(gridExtra)
+	
+	
+	# Set the plotting aesthetics
+	
+		plot_themeCML <-theme(panel.grid = element_blank(),  panel.background = element_rect(color="gray5"), panel.border = element_blank(), axis.text = element_text(colour="black"), axis.line = element_line(colour="black"), legend.background = element_rect(size=0.25, color="black"), legend.key = element_blank(),strip.background=element_blank(),strip.text.y=element_text(angle=0))
+	
+	
+	# Color palatte for the different sites
+		#clr<-c("#9FBF7D", "#FAA0E1", "#FA906D", "#76B4D5", "#45FEEB", "#B7C354", "#3FDBDE") 
+	
+	
+	# Create labels for the facets
+		#transect_labels<-list("2"="Transect 2","2.5"="Transect 2.5","3"="Transect 3","4"="Transect 4","flood"="flood","no flood"="no flood")
+		#transectlabeller<-function(variable,value){return(transect_labels[value])}
+	
+	t_labeller <-function(var,value){
+		value<-as.character(value)
+		if (var=="Transect"){
+			value[value=="2"] <- "Transect 2"
+			value[value=="2.5"] <- "Transect 2.5"
+			value[value=="3"] <- "Transect 3"
+			value[value=="4"] <- "Transect 4"
+		}
+		if (var=="Flood"){
+			value[value=="flood"] <- "flood"
+			value[value=="no flood"] <-"no flood"
+		}
+		return(value)
+	}
+	
+
+
+# Plot of Clad Height as a function of Irradiance		
+	pl<-ggplot(data=cml, aes(x=watt_avg,y=CladInt,group=Transect))
+  
+  	pl + geom_point(size=1) + facet_grid(Flood~Transect, scales='free_y', labeller=t_labeller) + labs(x="Irradiance (Avg. Watt Hours / Day)", y="Max. Clad Height (cm)") + ggtitle("Max. Clad. Height as a Function of Irradiance") + plot_themeCML
+  
+
+# Plot of Clad Height and Irradiance across Channel Cross Section	
+	p<-ggplot(data=cml, aes(x=xstrm,y=CladInt,group=Transect))
+
+
+	p2<-ggplot(data=CladMaxPointWet, aes(x=xstrm,y=CladInt,group=Transect))
+ 
+ 	p2 + geom_point() + geom_line(aes(x=xstrm,y=watt_avg/5), color="blue") + facet_grid(Flood~Transect, scales='free_y')
+
